f94fcabbe4b2835e1dbc7c33509c56e8e4d36904
Yuriy Zhuk <zhukyuri@gmail.com>
2022-02-16 12:31:37 +0200

    Connect to Mongo DB

--- server/index.ts
+++ server/index.ts
@@ -2,14 +2,22 @@ import "dotenv/config";
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+// @ts-ignore
+import mongoose from "mongoose";
 
 
 const PORT = process.env.PORT || 5001;
+const DB_URL = process.env.DB_URL || '';
 
 const app = express();
 
-const start = () => {
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+
+const start = async () => {
   try {
+    await mongoose.connect(DB_URL );
     app.listen( PORT, () => console.log(`Server started on PORT = ${PORT}`))
   } catch (e) {
     console.log(e)
