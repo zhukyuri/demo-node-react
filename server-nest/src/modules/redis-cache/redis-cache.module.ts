1f87e6dbaeb57f6a3a2f759b18da4ee45293ffa2
Yuriy Zhuk <zhukyuri@gmail.com>
2022-03-31 21:59:13 +0300

    [nest] Remove redis-cache.controller.ts

--- server-nest/src/modules/redis-cache/redis-cache.module.ts
+++ server-nest/src/modules/redis-cache/redis-cache.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
 
 import { CacheModule } from './cache/cache.module';
-import { RedisCacheController } from './redis-cache.controller';
 import { RedisCacheService } from './redis-cache.service';
 
 @Module({
   imports: [CacheModule],
-  controllers: [RedisCacheController],
   providers: [RedisCacheService],
   exports: [RedisCacheService],
 })
