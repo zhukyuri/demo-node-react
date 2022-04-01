b08f289e254916924c888fdd2bff13ff723c3649
Yuriy Zhuk <zhukyuri@gmail.com>
2022-04-01 11:56:54 +0300

    [nest] Use Cors ans CookieParser

--- server-nest/src/main.ts
+++ server-nest/src/main.ts
@@ -1,4 +1,7 @@
 import { NestFactory } from '@nestjs/core';
+import * as cookieParser from 'cookie-parser';
+import * as cors from 'cors';
+import * as os from 'os';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './modules/app/app.module';
 import {
@@ -9,6 +12,13 @@ import {
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cookieParser());
+  app.use(
+    cors({
+      credentials: true,
+      origin: process.env.CLIENT_URL,
+    }),
+  );
 
   const config = new DocumentBuilder()
     .setTitle('Demo-Code example of NestJS')
@@ -21,6 +31,8 @@ async function bootstrap() {
 
   await app.listen(process.env.NODE_PORT);
   console.log(`\nServer run with port ${process.env.NODE_PORT}`);
+  console.log('\nCounts of processors:', os.cpus().length);
+  console.log('\nCORS to:', process.env.CLIENT_URL);
   console.log(
     `\nTokens expires:\n  access = ${msecToSecond(
       expiresAccessToken,
